212965b81ac9849b62d8ed001335496cc18e278d
Neromancy <andrewhermawan1705@gmail.com>
2025-06-30 14:24:41 +0700

    buinu

--- core/src/main/java/com/dimmingechoes/screens/EndingScreen.java
+++ core/src/main/java/com/dimmingechoes/screens/EndingScreen.java
@@ -1,4 +1,39 @@
 package com.dimmingEchoes.screens;
 
-public class EndingScreen {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class EndingScreen implements Screen {
+    private final String endingMessage;
+    private final SpriteBatch batch = new SpriteBatch();
+    private final BitmapFont font = new BitmapFont();
+
+    public EndingScreen(String endingMessage) {
+        this.endingMessage = endingMessage;
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        font.draw(batch, endingMessage, 50, Gdx.graphics.getHeight() - 50);
+        font.draw(batch, "Press ESC to exit.", 50, 100);
+        batch.end();
+
+        if (Gdx.input.isKeyPressed(com.badlogic.gdx.Input.Keys.ESCAPE)) {
+            Gdx.app.exit();
+        }
+    }
+
+    @Override public void resize(int width, int height) {}
+    @Override public void show() {}
+    @Override public void hide() {}
+    @Override public void pause() {}
+    @Override public void resume() {}
+    @Override public void dispose() { batch.dispose(); font.dispose(); }
 }
