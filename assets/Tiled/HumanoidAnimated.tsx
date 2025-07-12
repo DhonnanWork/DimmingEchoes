a20d7519ef1fbe2a8263915ac5f1f33ab16a12a6
Izzat <izzatulislam21@gmail.com>
2025-07-12 21:58:43 +0700

    Upload All

---
+++ assets/Tiled/HumanoidAnimated.tsx
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="HumanoidAnimated" tilewidth="16" tileheight="16" tilecount="432" columns="16">
+ <image source="../DawnLike/Characters/HumanoidAnimated.png" width="256" height="432"/>
+ <tile id="58">
+  <animation>
+   <frame tileid="58" duration="1000"/>
+   <frame tileid="50" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="234">
+  <animation>
+   <frame tileid="234" duration="1000"/>
+   <frame tileid="226" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="290">
+  <animation>
+   <frame tileid="290" duration="1000"/>
+   <frame tileid="298" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="377">
+  <animation>
+   <frame tileid="377" duration="1000"/>
+   <frame tileid="369" duration="1000"/>
+  </animation>
+ </tile>
+</tileset>
